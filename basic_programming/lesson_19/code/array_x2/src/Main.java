535f7936fb27f852144e6c133f7f1192599a9598
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-10 21:26:24 +0100

    Fix for lesson 19

--- basic_programming/lesson_19/code/array_x2/src/Main.java
+++ basic_programming/lesson_19/code/array_x2/src/Main.java
@@ -1,7 +1,4 @@
-package code;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
         System.out.println("Работа с 2-хмерным массивом.");
@@ -10,15 +7,16 @@ public class Main {
         int [][] field = new int[8][8];
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++){
-             if ((i + j) % 2 ==0 ){
-                 field[i][j]= 0;
-             }  else {
-                 field[i][j]= 1;
-             }
+                if ((i + j) % 2 ==0 ){
+                    field[i][j]= 0;
+                }  else {
+                    field[i][j]= 1;
+                }
 
                 System.out.print(field[i][j] + "\t");
             }
             System.out.println();
         }
+
     }
 }
