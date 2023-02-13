bb767c5e121d6fb3426634b25c939aad2a1d6955
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-14 00:14:14 +0100

    lesson 21

---
+++ basic_programming/lesson_21/code/nod/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Нахождение НОД для двух целых чисел a и b");
+    // Задача: найти наибольший общий делитель (НОД) двух целых чисел a и b.
+    //Примеры:
+    //15 и 20: НОД = 5
+    //15 и 60: НОД = 15
+    //17 и 23: НОД = 1
+    //24 и 40: НОД = 8
+    //6 и 18: НОД = 6
+
+    //Алгоритм:
+    //Шаг 1: сравнить a и b, найти меньшее из них, пусть меньшее будет min, а большее max.
+    //Шаг 2: пройти в цикле от 1 до min (включительно) и делить min на параметр цикла.
+    //Если min разделилось нацело на параметр цикла, то делим второе число max на это же число.
+    //Если max разделилось нацело на параметр цикла, то НОД = параметр цикла.
+
+
+
+    }
+}
+
+
+/*
+
+        Scanner scanner = new Scanner(System.in);
+        int nod = 0;
+
+        System.out.println("Введите целое число a: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите целое число b: ");
+        int b = scanner.nextInt();
+        int min = min (a, b);
+        System.out.println("Меньшее число = " + min);
+        int max = max (a, b);
+        System.out.println("Большее число = " + max);
+
+        for  (int i = 1; i <= min; i++){
+            if (min % i == 0){
+                if (max % i == 0) {
+                    nod = i;
+                }
+            }
+        }
+        System.out.println("НОД чисел a и b = " + nod );
+
+    }
+    public static int min (int num1, int num2){
+        if (num1 < num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+    public static int max (int num1, int num2){
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+ */
