0647873d12b2ec468a8aaf7213b31218ba985328
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-14 09:22:06 +0100

    lesson 21

---
+++ basic_programming/lesson_21/code/finder/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Поиск элемента массива");
+
+        // Задать массив случайных целыхх чисел от 1 до 10 включительно.
+        // Запросить у пользователя произвольное число от 1 до 10.
+        // Проверить, встретилось ли введенное число в массиве и сколько раз.
+        // Поиск в массиве реализовать как метод, на входе которого массив и искомое число, на выходе -
+        // какое количество раз оно встретилось.
+        //   ТИП[] имя_массива = new ТИП[размер];
+        // int n = (int)(( Math.random() * (b - a + 1) + a)) - генерирует случайное **целое** число в интервале [a, b]
+        //   a - может быть, b - может быть
+
+    }
+}
+
+/*  int[] myArray = new int[100];
+
+//    for (int i = 0; i < myArray.length; i++) {
+//        myArray[i] = (int)(Math.random()*(10 - 1 + 1) +1);
+//    }
+//        Scanner scann = new Scanner(System.in);
+//
+//        System.out.println("Задумайте целое число от 1 до 10 ");
+//        int num = scann.nextInt();
+//        System.out.println("Вы ввели число " + num);
+//        int c = find (myArray, num);
+//        System.out.println("Это число встретилось в массиве " + c + " раз.");
+//    }
+//    public static int find (int []array, int n) {
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] == n) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+*/
