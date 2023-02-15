bcb50acf4230921d087a06b7b25275d2f2aae1e9
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-15 01:19:48 +0100

    lesson 22

---
+++ basic_programming/lesson_22/code/arraylist_sort/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    }
+}
+
+
+
+
+
+
+/*         ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            int randomNum = (int)(( Math.random() * (10 - 1 + 1) + 1));
+            myNumbers.add(i, randomNum);
+        }
+        // печать списка как есть
+        for (int i : myNumbers) {
+            System.out.print(i + " | ");
+        }
+            System.out.println();
+
+        Collections.sort(myNumbers);  // Sort myNumbers
+
+        // печать списка после сортировки циклом for each
+        for (int i : myNumbers) {
+            System.out.print(i + " | ");
+        }
+
+ */
