23b5ea7c2d35c1962c969c616d4731646547bc9c
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-16 13:03:25 +0100

    lesson 23

--- basic_programming/lesson_23/code/examples/src/Main.java
+++ basic_programming/lesson_23/code/examples/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.*;
 import java.util.Collections;
 
 public class Main {
@@ -8,12 +9,63 @@ public class Main {
         System.out.println("Hello world!");
 
         // ArrayList<int> myArrayList = new ArrayList<int>();
+
         // Задаем значения значений ArrayList используя метод asList
         ArrayList<String> namesList = new ArrayList<String>(
-            Arrays.asList("Peter","Vladimir","Stepan"));
+            Arrays.asList ("Peter","Vladimir","Stepan", "Sasha", "Leonid"));
         System.out.println(namesList);
 
         System.out.println();
+
+        // Вывод ArrayList с помощью цикла for each
+        for (String st : namesList) {
+            System.out.println(st);
+        }
+
+        ArrayList<String> arr = new ArrayList<>(
+                Arrays.asList("1", "2", "3", "4", "5")
+        );
+        System.out.println(" -- Вывод исходного массива -- \n" + arr + "\n_______");
+        System.out.println(" -- Вывод массива в цикле -- ");
+// Ключевая часть начало:
+        for (String item : arr) {
+            item = "9999";
+            System.out.printf("%s | ", item);
+        }
+// Ключевая часть конец.
+        System.out.println("\n_______\n -- Исходный массив не изменился -- \n" + arr);
+
+        System.out.println("\n\n ------ Второй вариант ------ \n");
+        System.out.println(" -- Вывод исходного массива -- \n" + arr + "\n_______");
+        System.out.println(" -- Вывод массива в цикле -- ");
+// Ключевая часть начало:
+        int i = 0;
+        for (String item : arr) {
+            arr.set(i, "9999");
+            i++;
+            System.out.printf("%s | ", item);
+        }
+// Ключевая часть конец.
+        System.out.println("\n_______\n -- Исходный массив ИЗМЕНИЛСЯ -- \n" + arr);
+
+        // Обратная сортировка ArrayList
+        ArrayList<String> mylist = new ArrayList<String>();
+        mylist.add("practice");
+        mylist.add("code");
+        mylist.add("quiz");
+        mylist.add("lesson");
+        System.out.println("Original List : " + mylist);
+
+        Collections.reverse(mylist);
+
+        System.out.println("Reversed List: " + mylist);
+
+
+    }
+}
+
+/*
+     System.out.println();
 
         // Вывод ArrayList с помощью цикла for each
         for (String st : namesList) {
@@ -41,6 +93,4 @@ public class Main {
         Collections.reverse(mylist);
 
         System.out.println("Reversed List: " + mylist);
-
-    }
-}
+ */
