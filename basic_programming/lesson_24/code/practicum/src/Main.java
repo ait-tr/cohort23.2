fa9be28e3141534983193525538680f8f860e67a
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-17 13:12:51 +0100

    lesson 24

--- basic_programming/lesson_24/code/practicum/src/Main.java
+++ basic_programming/lesson_24/code/practicum/src/Main.java
@@ -2,10 +2,30 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // Задача 1. Вывести слово задом наперед. Пример:
-        //Дано слово "троллейбус"
-        //Результат работы программы "cубйеллорт"
+        System.out.println("Разделить предложение по словам и записать их в ArrayList :");
+        // Предложение: Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему.
+        // На входе - Предложение
+        // На выходе - распечатать ArrayList, в котором содержаться слова из предложения
+        // Интерфейст - вырожденный (минимум)
+        // Ключевой алгоритм - идем по строке и ищем "пробел",
+        // когда найден пробел count++ , количество слов = count + 1.
+
+        String sentence = "Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему.";
+
+        String[] s = sentence.split(" "); // пробел указан в кавычках!!
+
+        for ( int i = 0 ; i < s.length  ; i++ ) {
+            System.out.print(s[i] + " | ");
+        }
+        System.out.println();
+
+        ArrayList<String> sentenceWords = new ArrayList<String>();
+        for ( int i=0 ; i <  s.length ; i++ ) {
+            sentenceWords.add(i , s[i]);
+        }
+        System.out.print(sentenceWords);
+    }
+}
 
 
 
@@ -14,7 +34,14 @@ public class Main {
 
 
 
-/*      Задача 1.
+
+
+
+
+
+
+
+/*      Задача 1. - решена Леонидом
         String word = "троллейбус"; // задаем слово
         int lengthOfWord = word.length(); // определяем длину слова
         System.out.println("Длина слова = " + lengthOfWord); // вывод длины слова
@@ -35,6 +62,48 @@ public class Main {
             lettersRev.add(i, st);  // заполняем лист буквами в обратном порядке
         }
         System.out.println(lettersRev); //печать листа
+
+       //Вывести слово задом наперед. Пример: решена на уроке
+        //На входе - Дано слово "троллейбус"
+        //На выходе - Результат работы программы "cубйеллорт"
+        // Что сделать - перставить буквы в слове
+        // Ключевой алгоритм - "разобрать" слово по буквам
+        // "Развернуть" массив задом наперд:
+        // Берем последнюю букву и записываем ее в начало НОВОГО массива (на 0 индекс)
+        // Предпоследнюю букву надо будет записать на индекс 1
+        // Предпредпоследнюю надо буде записать на индекс 2
+        // и т.д.
+        // в новом массиве
+        // используем для этого цил for
+        //  ArrayList<String> letters = new ArrayList<String>();
+
+        String word = "троллейбус";
+        System.out.println(word);
+
+        int lengthOfWord = word.length();
+        System.out.println("Длина слова " + lengthOfWord + "символов.");
+
+        ArrayList<Character> letters = new ArrayList<>();
+
+        for ( int i = 0  ; i < lengthOfWord ; i++ ) { // цикл по длине слова
+            // присвоить 0 элементу массива первую быкву
+            // присвоить 1 элементу массива вторую букву
+            // и т.д.
+            letters.add( i, Character.valueOf(word.charAt(i))); // отрываем от word по одной букве
+        }
+        System.out.println(letters); // печать листа
+
+        ArrayList<Character> lettersReverce = new ArrayList<>();
+
+        for ( int i = 0 ; i < lengthOfWord   ; i++ ) {
+            lettersReverce.add(  i,  letters.get(lengthOfWord - 1 - i));
+        }
+        System.out.println("Слово задом наперед ");
+        System.out.println( lettersReverce); // печать листа
+
+        for ( char ch: lettersReverce ) {
+            System.out.print(ch);
+        }
 
         // Задача 2. Подсчитать количество слов в предложении, занести слова из
         // предложения в ArrayList (слова разделены пробелами, знаки препинания не учитываются)
