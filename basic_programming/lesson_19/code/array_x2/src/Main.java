26a93273fe31e443c62ce6e4d8548219dfd60f55
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-02-18 17:32:22 +0100

    lesson update

--- basic_programming/lesson_19/code/array_x2/src/Main.java
+++ basic_programming/lesson_19/code/array_x2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Работа с 2-хмерным массивом.");
         //System.out.println();
         System.out.println("Шахматная доска:");
-        int [][] field = new int[8][8];
+        int [][] field = new int[8][8]; // инициализация массива
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++){
                 if ((i + j) % 2 ==0 ){
@@ -13,7 +13,7 @@ public class Main {
                     field[i][j]= 1;
                 }
 
-                System.out.print(field[i][j] + "\t");
+                System.out.print(field[i][j] + "\t"); //  /t - это отступ на размер табуляции
             }
             System.out.println();
         }
