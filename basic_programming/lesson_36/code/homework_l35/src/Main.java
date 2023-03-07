0bfc2619eb38bc8fe9ad1d60893885682edffac0
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-03-07 10:52:06 +0100

    lesson update

--- basic_programming/lesson_36/code/homework_l35/src/Main.java
+++ basic_programming/lesson_36/code/homework_l35/src/Main.java
@@ -3,39 +3,35 @@ public class Main {
         System.out.println("Работаем с классами");
 
         Wine myWine1 = new Wine();
-        myWine1.name = "Casillero del Diabolo";
-        myWine1.sort = "Каберне";
-        myWine1.year = 2016;
+        myWine1.name = "ЛяЛя Вино";
+        myWine1.sort = "каберне";
+        myWine1.year = 1994;
 
         Wine myWine2 = new Wine();
-        myWine2.sort = "Petrus";
-        myWine2.name = "Мерло";
-        myWine2.year = 2022;
+        myWine2.name = "Тополя Вино";
+        myWine2.sort = "мерло";
+        myWine2.year = 2004;
 
         Wine myWine3 = new Wine();
-        myWine3.name = "Rioja Vega";
-        myWine3.sort = "Риоха";
-        myWine3.year = 2019;
+        myWine3.name = "Киндзмараули";
+        myWine3.sort = "саперави";
+        myWine3.year = 1987;
 
-        System.out.println(myWine1.toString());
-        System.out.println(myWine2.toString());
-        System.out.println(myWine3.toString());
-
-        System.out.println(myWine1.onStock(3));
-        System.out.println(myWine2.onStock(2));
-        System.out.println(myWine3.onStock(5));
+       System.out.println(myWine1.onStock(10));
+       System.out.println(myWine2.onStock(20));
+       System.out.println(myWine3.onStock(25));
 
         System.out.println(myWine1.getYear());
         System.out.println(myWine2.getYear());
         System.out.println(myWine3.getYear());
 
 
-        Pet pet1 = new Pet("кошка", "британка", "Василиса", 4 );
-        Pet pet2 = new Pet("собака", "пудель", "Артемон", 10 );
-
-        System.out.println("Мои домашние питомцы: ");
-        System.out.println(pet1);
-        System.out.println(pet2);
+//        Pet pet1 = new Pet("кошка", "британка", "Василиса", 4 );
+//        Pet pet2 = new Pet("собака", "пудель", "Артемон", 10 );
+//
+//        System.out.println("Мои домашние питомцы: ");
+//        System.out.println(pet1);
+//        System.out.println(pet2);
 
     }
 }
--- basic_programming/lesson_36/code/homework_l35/src/Wine.java
+++ basic_programming/lesson_36/code/homework_l35/src/Wine.java
@@ -3,32 +3,20 @@ public class Wine {
     String sort;
     int year;
 
-    @Override
-    public String toString() {
+    @Override // переопределили метод
+    public String toString() { // метод печатает в строку все поля класса
         return "Wine{" +
                 "name='" + name + '\'' +
                 ", sort='" + sort + '\'' +
                 ", year=" + year +
                 '}';
-
     }
 
-    public int onStock(int n) {
-        System.out.print("На складе имеется бутылок: " );
-        return n;
+    public String onStock(int n) {
+        return ("На складе имеется " +  n + " бутылок");
     }
 
     public int getYear() {
         return year;
     }
 }
-
-//        Создайте класс Wine, который содержит переменные name, sort и year.
-//        Создайте три экземпляра этого класса.
-//        Выведите на консоль значения их переменных методом toString.
-
-//        Добавить в класс Wine метод: onStock, имеет один параметр – количество бутылок.
-//        Выводит на консоль сообщение "На складе имеется количество бутылок".
-
-//        Задайте метод getYear – возвращает год производства вина.
-//        Вызвать этот метод для каждого из объектов.
