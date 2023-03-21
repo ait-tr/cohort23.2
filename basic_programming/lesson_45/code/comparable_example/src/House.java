f98334941637e7a0ed8fdbe5eb7736b817db541f
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-03-21 13:06:03 +0100

    lesson update

--- basic_programming/lesson_45/code/comparable_example/src/House.java
+++ basic_programming/lesson_45/code/comparable_example/src/House.java
@@ -1,33 +1,33 @@
-public class House implements Comparable<House> {
-    int area;
-    int price;
-    String city;
+public class House implements Comparable<House> { // можем сравнивать и сортировать объекты этого класса
+    int area; // площадь дома
+    int price; // цена
+    String city; // город
 
-
-    public House(int area, int price, String city, boolean hasFurniture)
+    // конструктор класса
+    public House(int area, int price, String city)
     {
         this.area = area;
         this.price = price;
         this.city = city;
     }
-
+    // переопределение метода toString
     @Override
     public String toString() {
         return "House{" +
                 "area=" + area +
                 ", price=" + price +
-                ", city='" + city + '\'' +
-                '}';
+                ", city='" + city +
+                '}' + '\n' ;
     }
 
-    @Override
-    public int compareTo(House anotherHouse) {
-        if (this.area == anotherHouse.area) {
-            return 0;
-        } else if (this.area < anotherHouse.area) {
-            return -1;
+    @Override // директива переопредеоения
+    public int compareTo(House anotherHouse) { // сигнатура метода: тип возвращаемого значения, наименование метода и параметры вызова
+        if (this.price == anotherHouse.price) { // условие сравнения по площади дома, это математическое сранение целых чисел
+            return 0; // возвращаем 0, если площади равны
+        } else if (this.price < anotherHouse.price) {
+            return -1; // возвращаем -1, если одна площадь меньще другой
         } else {
-            return 1;
+            return 1; // возвращаем -1, если одна площадь меньще другой
         }
     }
 
